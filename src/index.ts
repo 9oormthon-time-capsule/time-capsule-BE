bcd840a7eac5b6cc6b4468cbc980946399e40ad1
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-01-06 10:07:07 +0900

    feat: 편지 등록 및 조회 API 구현 (#5)

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import itemRoutes from './router/routerExample';
+import letterRoutes from './router/routerLetter';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -7,12 +8,13 @@ const port = process.env.PORT || 4000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello, Time Capsule!');
+	res.send('Hello, Time Capsule!');
 });
 
 // 예시 라우터 사용
 app.use('/api', itemRoutes);
+app.use('/api', letterRoutes);
 
 app.listen(port, () => {
-  console.log(`서버 실행 성공! http://localhost:${port}`);
+	console.log(`서버 실행 성공! http://localhost:${port}`);
 });

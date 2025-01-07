44abedaa16c306e258dd80e852e4ba7bc7107b11
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-01-07 16:00:02 +0900

    [REFACTOR] refactor: 카테고리 API 구조 변경 (#18)
    * refactor: 카테고리 api 수정
    
    * chore: 변수명 통일

--- src/router/routerCategory.ts
+++ src/router/routerCategory.ts
@@ -3,21 +3,33 @@ import { addCategory, getCategories } from "../firebase/firebaseCategory";
 
 const router = Router();
 
-router.post("/todo/category/:userId", async (req, res) => {
-  const { categoryName, color } = req.body;
-  const { userId } = req.params;
+router.post("/todo/category", async (req, res) => {
+  const userId = req.session.userData?._id;
+  const { categoryName, textColor } = req.body;
+
+  if (!userId) {
+    res.status(401).send("로그인이 필요합니다.");
+    return;
+  }
 
   try {
-    const categoryId = await addCategory(userId, categoryName, color);
+    const categoryId = await addCategory(userId, categoryName, textColor);
     res.status(201).json({ message: "카테고리가 추가되었습니다.", categoryId });
   } catch (error: any) {
     res.status(500).send(error.message);
   }
 });
 
-router.get("/todo/category/:userId", async (req, res) => {
+router.get("/todo/category", async (req, res) => {
+  const userId = req.session.userData?._id;
+
+  if (!userId) {
+    res.status(401).send("로그인이 필요합니다.");
+    return;
+  }
+
   try {
-    const categories = await getCategories();
+    const categories = await getCategories(userId);
     res.status(200).json(categories);
   } catch (error: any) {
     res.status(500).send(error.message);

8f7373c97ecebfdab9290042bbf0d593f5c58129
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-01-10 12:21:34 +0900

    [FEAT] feat: Todo 등록, 조회, 삭제 및 checkbox 상태 업데이트 API 구현 (#25)
    * chore: firebaseTodo.ts, routerTodo.ts 파일 생성
    
    * feat: Todo 추가, 조회, 수정 api 구현
    
    * refactor: todo 등록 시 불필요한 파라미터 제거
    
    * feat: 삭제 api 구현

---
+++ src/firebase/firebaseTodo.ts
@@ -0,0 +1,80 @@
+import { database } from "../config/firebaseConfig";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  serverTimestamp,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
+// Todo 등록 함수
+export const addTodo = async (
+  userId: number,
+  task: string,
+  categoryId: string
+) => {
+  try {
+    const docRef = await addDoc(
+      collection(doc(database, "todos", userId.toString()), "posts"),
+      {
+        task,
+        categoryId,
+        isCompleted: false,
+        createdAt: serverTimestamp(),
+      }
+    );
+
+    return docRef.id;
+  } catch (error) {
+    throw new Error(`할 일 등록 중 오류 발생: ${error}`);
+  }
+};
+
+// todo 조회 함수
+export const getTodo = async (userId: number) => {
+  try {
+    const querySnapshot = await getDocs(
+      collection(doc(database, "todos", userId.toString()), "posts")
+    );
+    const todos = querySnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    return todos;
+  } catch (error) {
+    throw new Error(`할 일 조회 중 오류 발생: ${error}`);
+  }
+};
+
+// todo update 함수
+export const updateTodo = async (
+  userId: number,
+  todoId: string,
+  isCompleted: boolean
+) => {
+  try {
+    await updateDoc(
+      doc(database, "todos", userId.toString(), "posts", todoId),
+      {
+        isCompleted,
+      }
+    );
+
+    return "todo update 완료";
+  } catch (error) {
+    throw new Error(`할 일 업데이트 중 오류 발생: ${error}`);
+  }
+};
+
+export const deleteTodo = async (userId: number, todoId: string) => {
+  try {
+    await deleteDoc(doc(database, "todos", userId.toString(), "posts", todoId));
+
+    return "todo 삭제 완료";
+  } catch (error) {
+    throw new Error(`할 일 삭제 중 오류 발생: ${error}`);
+  }
+};
---
+++ src/router/routerTodo.ts
@@ -0,0 +1,79 @@
+import { Router } from "express";
+import {
+  addTodo,
+  deleteTodo,
+  getTodo,
+  updateTodo,
+} from "../firebase/firebaseTodo";
+
+const router = Router();
+
+router.post("/todo/task", async (req, res) => {
+  const userId = req.session.userData?._id; // 세션에서 사용자 ID 가져옴
+  const { task, categoryId } = req.body;
+
+  if (!userId) {
+    res.status(401).send("로그인이 필요합니다.");
+    return;
+  }
+
+  try {
+    const todoId = await addTodo(userId, task, categoryId);
+    res.status(201).send(`할 일이 등록되었습니다. ID: ${todoId}`);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.get("/todo/task", async (req, res) => {
+  const userId = req.session.userData?._id;
+
+  if (!userId) {
+    res.status(401).send("로그인이 필요합니다.");
+    return;
+  }
+
+  try {
+    const todos = await getTodo(userId); // 세션 ID를 사용
+    res.status(200).json(todos);
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.patch("/todo/task/:todoId", async (req, res) => {
+  const userId = req.session.userData?._id;
+  const { todoId } = req.params;
+  const { isCompleted } = req.body;
+
+  if (!userId) {
+    res.status(401).send("로그인이 필요합니다.");
+    return;
+  }
+
+  try {
+    await updateTodo(userId, todoId, isCompleted);
+    res.status(200).send("할 일이 수정되었습니다.");
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.delete("/todo/task/:todoId", async (req, res) => {
+  const userId = req.session.userData?._id;
+  const { todoId } = req.params;
+
+  if (!userId) {
+    res.status(401).send("로그인이 필요합니다.");
+    return;
+  }
+
+  try {
+    await deleteTodo(userId, todoId);
+    res.status(200).send("할 일이 삭제되었습니다.");
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+});
+
+export default router;

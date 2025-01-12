5c5ace9cf863b7f806bcc90807c5f8ccc7a406c0
sohee <106158901+ccconac@users.noreply.github.com>
2025-01-12 15:02:35 +0900

    fix: 카카오 로그인 props type error 해결 (#32)

--- src/router/kakaoRoutes.ts
+++ src/router/kakaoRoutes.ts
@@ -15,59 +15,56 @@ const kakao = {
 };
 
 /* login 이후 나타나는 callback page */
-kakaoRouter.get(
-  "/oauth/callback/kakao",
-  async (req: Request, res: Response) => {
-    /* access token 발급 */
-    let token: any;
-    try {
-      token = await axios({
-        method: "POST",
-        url: "https://kauth.kakao.com/oauth/token",
-        headers: {
-          "content-type": "application/x-www-form-urlencoded",
-        },
-        data: qs.stringify({
-          grant_type: "authorization_code",
-          client_id: kakao.CLIENT_ID,
-          redirect_uri: kakao.REDIRECT_URI,
-          code: req.query.code as string,
-        }),
-      });
-    } catch (error: any) {
-      console.error("토근을 불러올 수 없습니다.", error);
-      return res.status(400).json({ error: "토큰 발급 실패" });
-    }
+kakaoRouter.get("/oauth/callback/kakao", async (req: any, res: any) => {
+  /* access token 발급 */
+  let token: any;
+  try {
+    token = await axios({
+      method: "POST",
+      url: "https://kauth.kakao.com/oauth/token",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      data: qs.stringify({
+        grant_type: "authorization_code",
+        client_id: kakao.CLIENT_ID,
+        redirect_uri: kakao.REDIRECT_URI,
+        code: req.query.code as string,
+      }),
+    });
+  } catch (error: any) {
+    console.error("토근을 불러올 수 없습니다.", error);
+    return res.status(400).json({ error: "토큰 발급 실패" });
+  }
 
-    /* access token 발급받은 뒤 사용자 정보 가져옴 */
-    let user: any;
-    try {
-      user = await axios({
-        method: "GET",
-        url: "https://kapi.kakao.com/v2/user/me",
-        headers: {
-          Authorization: `Bearer ${token.data.access_token}`,
-        },
-      });
-    } catch (error: any) {
-      console.error("유저 데이터를 불러올 수 없습니다.", error);
-      return res.status(400).json({ error: "사용자 정보 조회 실패" });
-    }
+  /* access token 발급받은 뒤 사용자 정보 가져옴 */
+  let user: any;
+  try {
+    user = await axios({
+      method: "GET",
+      url: "https://kapi.kakao.com/v2/user/me",
+      headers: {
+        Authorization: `Bearer ${token.data.access_token}`,
+      },
+    });
+  } catch (error: any) {
+    console.error("유저 데이터를 불러올 수 없습니다.", error);
+    return res.status(400).json({ error: "사용자 정보 조회 실패" });
+  }
 
-    /* 가지고 온 사용자 정보 DB & session 저장 */
-    await setUsers(user.data);
+  /* 가지고 온 사용자 정보 DB & session 저장 */
+  await setUsers(user.data);
 
-    req.session.userData = {
-      _id: user.data.id,
-      name: user.data.kakao_account.profile.nickname,
-      profileImage: user.data.kakao_account.profile.profile_image_url,
-      accessToken: token.data.access_token,
-    };
+  req.session.userData = {
+    _id: user.data.id,
+    name: user.data.kakao_account.profile.nickname,
+    profileImage: user.data.kakao_account.profile.profile_image_url,
+    accessToken: token.data.access_token,
+  };
 
-    await req.session.save(); // 세션 저장
-    res.redirect(`http://localhost:3000/main`);
-  },
-);
+  await req.session.save(); // 세션 저장
+  res.redirect(`http://localhost:3000/main`);
+});
 
 /* session에 저장된 사용자 정보 Client 전달 */
 kakaoRouter.get("/api/user", async (req: Request, res: Response) => {

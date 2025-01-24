1c3e6ab818003a03c7c35dae6d17a9c44c76550b
kkyung <68538307+kkyung99@users.noreply.github.com>
2025-01-24 19:14:02 +0900

    fix: Todo 등록 API 날짜 처리 방식 수정 (#38)

--- src/router/routerTodo.ts
+++ src/router/routerTodo.ts
@@ -10,7 +10,7 @@ const router = Router();
 
 router.post("/todo/task", async (req, res) => {
   const userId = req.session.userData?._id; // 세션에서 사용자 ID 가져옴
-  const { task, categoryId } = req.body;
+  const { task, categoryId, selectedDate } = req.body;
 
   if (!userId) {
     res.status(401).send("로그인이 필요합니다.");
@@ -18,7 +18,7 @@ router.post("/todo/task", async (req, res) => {
   }
 
   try {
-    const todoId = await addTodo(userId, task, categoryId);
+    const todoId = await addTodo(userId, task, categoryId, selectedDate);
     res.status(201).send(`할 일이 등록되었습니다. ID: ${todoId}`);
   } catch (error: any) {
     res.status(500).send(error.message);
